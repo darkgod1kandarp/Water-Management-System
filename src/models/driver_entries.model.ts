1987d8dd27b87367c10dc5b7ad029c64defd9956
darkgod1kandarp <009kandarp@gmail.com>
2024-04-28 23:25:23 +0530

    Added bottle tally column in driver entries

--- src/models/driver_entries.model.ts
+++ src/models/driver_entries.model.ts
@@ -59,9 +59,9 @@ const Drivers =  sequelize.define('driver_entries', {
             hooks: {
                 beforeCreate: async (entry:any) => {
                     const customer:any =  await customerModel.findOne({where: {id: entry.customer_id}})  
-                    console.log(entry);  
                     if (customer) {
                         entry.bottle_tally = Number(customer.bottle_tally) + Number(entry.bottle_delivered) - Number(entry.bottle_received);
+                        console.log("Bottle Tally", entry.bottle_tally);
                         customer.bottle_tally = entry.bottle_tally; 
                         await customer.save();
                     } else {
